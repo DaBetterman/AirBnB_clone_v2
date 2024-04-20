23893acc239c5b3f42d0a29226bc1bce0a0d43d8
DaBetterman <1ebrahimr@gmail.com>
2024-04-20 21:19:20 +0200

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-from sys import argv
+"""
+A script that starts a Flask web application
+"""
 
-argv = int(argv[1])
+from flask import Flask, render_template
+from models import *
+from models import storage
 
-args = argv % 2
+app = Flask(__name__)
 
-if args == 1:
-    print("Number: {:d} is odd".format(argv))
-elif args == 0:
-    print("Number: {:d} is even".format(argv))
-else:
-    print("Not Found")
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    display a HTML page with the states listed in alphabetical order
+    """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
