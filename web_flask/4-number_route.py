cf3032c46ec261642c356a02a7fbe23cfde7a897
DaBetterman <1ebrahimr@gmail.com>
2024-04-20 15:23:16 +0200

    added more comments and remove "debug=True"

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-# A script that starts a Flask web application
-
+"""
+A script that starts a Flask web application
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """Returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Returns HBNB"""
     return 'HBNB'
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
+    """Returns 'C' + the argument content"""
     replacer = text.replace('_', ' ')
     return 'C ' + replacer
 
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python/", defaults={'text':'is cool'}, strict_slashes=False)
 def python(text):
+    """Returns 'Python' + the argument value, if no arg value
+        then it will return 'is cool' as a default value"""
     replacer = text.replace('_', ' ')
     return 'Python ' + replacer
 
@@ -29,4 +35,4 @@ def number(n):
         return f'{n} is a number'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
